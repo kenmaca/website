3647bf0879c2e6799318a51a31957fdf6adbe797
Kenneth Ma <ken@kenma.ca>
2017-04-26 22:34:06 -0400

    Completed: Added first iteration of website, still WIP

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(() => {
+
+  // animate intro
+  tl = new TimelineMax();
+  tl.to($('#cover'), 0.5, {opacity: 0.1}, 'startIntro')
+    .to($('#intro h1:nth-child(1)'), 0.5, {ease: Expo.easeInOut, opacity: 1, y: -50}, 'startIntro')
+    .to($('#intro h1:nth-child(2)'), 0.5, {ease: Expo.easeInOut, opacity: 1, y: -50})
+    .to($('#intro h1:nth-child(3)'), 0.5, {ease: Expo.easeInOut, opacity: 1, y: -50}, 'endIntro')
+    .to($('#intro .divider'), 0.5, {ease: Expo.easeInOut, opacity: 1, y: -50}, 'endIntro')
+    .to($('#signature'), 0.5, {opacity: 0.9})
+    .to($('header ul'), 0.5, {ease: Expo.easeInOut, opacity: 1, transform: 'translateY(1vh)'});
+
+  sm = new ScrollMagic.Controller();
+  console.log('test');
+  header = new ScrollMagic.Scene({
+    offset: 0,
+    duration: 100
+  }).addTo(sm)
+    .on('update', event => console.log(event));
+});
